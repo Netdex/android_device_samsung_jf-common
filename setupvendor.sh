51f204d3e9c5421f13148fe39c14bbbfcc5b04a7
netdex <netdex593@gmail.com>
2017-01-04 11:26:57 -0500

    Alternative proprietary blob generation

--- setupvendor.sh
+++ setupvendor.sh
@@ -1,7 +1,7 @@
-DEVICE=jfltexx
-VENDOR=samsung
-DEVICE_COMMON=jf-common
-MY_DIR=`pwd`
-BLOB_LOC=jf-gsm-common
+export DEVICE=jfltexx
+export VENDOR=samsung
+export DEVICE_COMMON=jf-common
+export MY_DIR=`pwd`
+export BLOB_LOC=jf-gsm-common
 
 ./setup-makefiles.sh
